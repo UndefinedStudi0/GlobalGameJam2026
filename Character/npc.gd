9bee5976846c85db81db80f3185ed53cd8ccc979
Namlin <6908951+Arpic@users.noreply.github.com>
2026-01-31 18:11:46 +0100

    Add waypoint to jiggling mask

--- Character/npc.gd
+++ Character/npc.gd
@@ -9,6 +9,7 @@ var maskref = null
 var isAttached = false
 var stop_distance = 10
 var throw_time = 0
+var jiggling = false
 
 func _ready() -> void:
 	add_to_group("npc")
@@ -18,7 +19,6 @@ func _physics_process(delta):
 
 	if !is_in_attach_grace_period():
 		set_collision_mask_value(2, true)
-
 
 	if Input.is_action_just_pressed("throw") && isAttached:
 		print("throwing")
@@ -55,12 +55,19 @@ func check_for_mask():
 		if mask.get("name") == "Mask" && !is_in_attach_grace_period():
 			isAttached = mask.attach(self, collisionShape)
 			if isAttached:
+				if jiggling:
+					waypoints.remove_at(0)
+					jiggling = false
 				maskref = mask
 
 	update_facing_direction()
 
 func reach_waypoint():
-	current_waypoint = (current_waypoint+1) % waypoints.size()
+	if jiggling:
+		waypoints.remove_at(0)
+		jiggling = false
+	else:
+		current_waypoint = (current_waypoint+1) % waypoints.size()
 	print("reached next waypoint")
 
 func is_in_attach_grace_period():
@@ -75,6 +82,10 @@ func throw():
 	maskref = null
 	
 func getJigglyWith(mask):
-	isAttached = mask.attach(self, collisionShape)
-	if isAttached:
-		maskref = mask
+	if jiggling:
+		return
+	jiggling = true
+	var maskWayPoint = Marker2D.new()
+	maskWayPoint.global_position = self.to_local(mask.global_position)
+	add_child(maskWayPoint)
+	waypoints.insert(0, maskWayPoint)
