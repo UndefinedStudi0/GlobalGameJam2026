3664c3708c39ddb79a90c5088b2fb04f18d9d8b3
Gab_Sch <schedergabriel@gmail.com>
2026-01-31 22:50:59 +0100

    update node structure

--- Levels/level_1.gd
+++ Levels/level_1.gd
@@ -6,19 +6,19 @@ const DOOR_OPENED_CHAT_BOX_ID = "door-opened"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	InteractionGroups.addInteractionGroup($Node2D3/NpcY1, "yellow_door")
+	InteractionGroups.addInteractionGroup($Part1/Node2D3/NpcY1, "yellow_door")
 	# required so it can be detected by the blue door
-	$Node2D3/NpcY1.set_collision_layer_value(12, true)
+	$Part1/Node2D3/NpcY1.set_collision_layer_value(12, true)
 	
 	# example chat box closed detection event
 	SignalBus.chat_box_closed.connect(_on_need_to_open_the_door_chat_box_close)
 	
 	# set detection layer 12 (Mask)
-	$DoorOpenedArea2D.set_collision_mask_value(12, true)
+	$Part1/DoorOpenedArea2D.set_collision_mask_value(12, true)
 	
 	# connect the body entered event
-	if not $DoorOpenedArea2D.body_entered.is_connected(_on_close_door_opened_area_2d_body_entered):
-		$DoorOpenedArea2D.body_entered.connect(_on_close_door_opened_area_2d_body_entered)
+	if not $Part1/DoorOpenedArea2D.body_entered.is_connected(_on_close_door_opened_area_2d_body_entered):
+		$Part1/DoorOpenedArea2D.body_entered.connect(_on_close_door_opened_area_2d_body_entered)
 
 func _on_need_to_open_the_door_chat_box_close(type: String):
 	if type == DOOR_OPENED_CHAT_BOX_ID:
