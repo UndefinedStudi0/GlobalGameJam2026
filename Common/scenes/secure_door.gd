158d937c9ab3ad34f823f4ce3052c917c39ddc6e
André Videla <andre.videla@gmail.com>
2026-02-01 16:14:53 +0100

    add callback for secure door

--- Common/scenes/secure_door.gd
+++ Common/scenes/secure_door.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 var door_opened: bool = false
 
+var animation_end_callback = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	match door_colorid:
@@ -17,7 +19,8 @@ func _ready() -> void:
 		_:
 			$KeypassGreen.visible = false
 			$KeypassYellow.visible = false
-			
+	$AnimationPlayer.animation_finished.connect(animation_ended)
+
 	$Area2D.set_collision_mask_value(12, true)
 	InteractionGroups.addInteractionGroup(self, door_colorid)
 	# Ensure the signal is connected; it's not wired in the scene.
@@ -28,13 +31,20 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+	
+func animation_ended():
+	print("animation ended")
+	if animation_end_callback:
+		animation_end_callback.call()
 
 func _on_body_entered(body: Node) -> void:
 	if !door_opened or !door_opensonce:
 		if InteractionGroups.canInteractWith(self, body):
-			print("Can interact with:", body.name)
+			print("Can interact with2:", body.name)
 			$AnimationPlayer.play("DoorOpens")
 			door_opened = true
+
+			await get_tree().create_timer(2.0).timeout
+			animation_ended()
 		
 		# run interaction logic
